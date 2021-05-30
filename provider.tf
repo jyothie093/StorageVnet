0be6fe07302529c312835ea831774f1a72296113
jyothie093 <83720166+jyothie093@users.noreply.github.com>
2021-05-30 21:06:39 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,9 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "8cd0064b-e1ef-4efb-8f73-f6d61e17a953"
-  client_id       = "13380c5f-6eb2-416b-859c-41afaa17209c"
-  client_secret   = "__WjYWOJxH.M1TqPS2am81tp7.~mbL8y-T"
-  tenant_id       = "2dd5a12a-1432-4f77-b35b-946f47598573"
+
 }
 
 terraform {
